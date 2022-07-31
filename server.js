461a86c2a308ea9563ce09a573a74cb2dd501d31
rfabreu <raabreugomes@gmail.com>
2022-07-31 19:36:57 -0400

    Fixed SERVER routing issue not serving CSS file

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
-const { json } = require('body-parser');
 const express = require('express');
-const fs = require('fs');
-const { dirname } = require('path');
-const path = require('path');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
-const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 
@@ -15,13 +10,14 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
-// instructs the server to use the specified routes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
 
 // instructs the server to serve additional resources (css, js, images) with the html file
 app.use(express.static('public'));
 
+
+// instructs the server to use the specified routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => {
